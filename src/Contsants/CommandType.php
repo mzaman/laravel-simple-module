d9d63905bc609e955452619ca4e51e33916e5852
masud.zmn <masud.zmn@gmail.com>
2023-12-16 00:50:10 +0600

    Add CommandType constant

---
+++ src/Contsants/CommandType.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace LaravelSimpleModule\Constants;
+
+/**
+ * Class CommandType
+ *
+ * Defines constants for different types of command or argument formats.
+ */
+class CommandType
+{
+    /**
+     * Represents the Artisan command format.
+     */
+    const ARTISAN = 'artisan';
+
+    /**
+     * Represents the Symfony command format.
+     */
+    const SYMFONY = 'symfony';
+
+    /**
+     * Represents the Shell command format.
+     */
+    const SHELL = 'shell';
+}
