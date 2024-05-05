b3fa15513a9f609b9c4aee167ba8e6ccee9631a5
mzaman <masud.zmn@gmail.com>
2024-05-06 00:49:23 +0600

    Add additional methods to the base interfaces

--- src/Interfaces/DataInterface.php
+++ src/Interfaces/DataInterface.php
@@ -4,7 +4,7 @@ namespace LaravelSimpleModule\Interfaces;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface DataInterface
 {
@@ -12,7 +12,7 @@ interface DataInterface
      * Find an item by id.
      *
      * @param mixed $id
-     * @return Model|null
+     * @return \Illuminate\Database\Eloquent\Model|null
      */
     public function find($id);
 
@@ -20,19 +20,19 @@ interface DataInterface
      * Find an item by id or fail.
      *
      * @param mixed $id
-     * @return Model
+     * @return \Illuminate\Database\Eloquent\Model
      */
     public function findOrFail($id);
 
     /**
-     * Return all items.
+     * Get all the model records in the database.
      *
-     * @return Collection|null
+     * @return \Illuminate\Database\Eloquent\Collection
      */
     public function all();
 
     /**
-     * Count the number of items.
+     * Count the number of specified model records in the database.
      *
      * @return int
      */
@@ -42,7 +42,7 @@ interface DataInterface
      * Create an item.
      *
      * @param array $data
-     * @return Model|null
+     * @return \Illuminate\Database\Eloquent\Model|null
      */
     public function create(array $data);
 
@@ -50,19 +50,22 @@ interface DataInterface
      * Create multiple items.
      *
      * @param array $data
-     * @return Collection
+     * @return \Illuminate\Database\Eloquent\Collection
      */
     public function createMultiple(array $data);
 
     /**
      * Delete all items.
+     *
+     * @return bool|null
      */
     public function delete();
 
     /**
-     * Delete an item by id.
+     * Delete the specified model record from the database.
      *
      * @param mixed $id
+     * @return bool|null
      */
     public function deleteById($id);
 
@@ -75,29 +78,54 @@ interface DataInterface
     public function deleteMultipleById(array $ids);
 
     /**
-     * Get the first item.
+     * Get the first specified model record from the database.
      *
-     * @return Model|null
+     * @return \Illuminate\Database\Eloquent\Model
      */
     public function first();
 
     /**
-     * Get all items.
+     * Get the first specified model record from the database or throw an exception if not found.
      *
-     * @return Collection|null
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function firstOrFail();
+
+    /**
+     * Get all the specified model records in the database.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
      */
     public function get();
 
     /**
-     * Get an item by id.
+     * Get the specified model record from the database by its ID.
      *
      * @param mixed $id
-     * @return Model|null
+     * @return \Illuminate\Database\Eloquent\Model|null
      */
     public function getById($id);
 
     /**
-     * Limit the number of items.
+     * Get the specified model record from the database by its ID or throw an exception if not found.
+     *
+     * @param mixed $id
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getByIdOrFail($id);
+
+    /**
+     * Get an item by column value.
+     *
+     * @param mixed $item
+     * @param string $column
+     * @param array $columns
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function getByColumn($item, $column, array $columns = ['*']);
+
+    /**
+     * Set the query limit.
      *
      * @param int $limit
      * @return $this
@@ -105,25 +133,36 @@ interface DataInterface
     public function limit($limit);
 
     /**
-     * Order the items.
+     * Set an ORDER BY clause.
      *
      * @param string $column
-     * @param string $value
+     * @param string $direction
      * @return $this
      */
-    public function orderBy($column, $value);
+    public function orderBy($column, $direction = 'asc');
 
     /**
      * Update an item by id.
      *
      * @param mixed $id
      * @param array $data
-     * @return bool
+     * @return \Illuminate\Database\Eloquent\Model
      */
     public function updateById($id, array $data);
 
     /**
-     * Add a where clause.
+     * Paginate the specified model records.
+     *
+     * @param int $limit
+     * @param array $columns
+     * @param string $pageName
+     * @param int|null $page
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function paginate($limit = 25, array $columns = ['*'], $pageName = 'page', $page = null);
+
+    /**
+     * Add a simple where clause to the query.
      *
      * @param string $column
      * @param mixed $value
@@ -133,7 +172,7 @@ interface DataInterface
     public function where($column, $value, $operator = '=');
 
     /**
-     * Add a where in clause.
+     * Add a simple where in clause to the query.
      *
      * @param string $column
      * @param mixed $values
@@ -142,7 +181,7 @@ interface DataInterface
     public function whereIn($column, $values);
 
     /**
-     * Add eager loading.
+     * Set Eloquent relationships to eager load.
      *
      * @param string|array $relations
      * @return $this
--- src/Interfaces/RepositoryInterface.php
+++ src/Interfaces/RepositoryInterface.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use LaravelSimpleModule\Interfaces\DataInterface;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface RepositoryInterface extends DataInterface
 {
